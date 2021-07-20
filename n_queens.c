ff5199f26196cfa5108bcd24f28c3d2f77ae943f
Rodolpho Cammarosano <rcammaro@student.s19>
2021-07-20 21:39:30 +0200

    changed calls to write to simply printf in print_array function

---
+++ n_queens.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	print_array(int *array, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+		printf("%X", array[i++]);
+	printf("\n");
+}
+
+int		check(int *array, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		if (array[i] == array[j]) // repeated line
+			return (0);
+		if (array[i] + i == array[j] + j) // descending diagonal
+			return (0);
+		if (array[i] - i == array[j] - j) // ascending diagonal
+			return (0);
+		j++;
+	}
+	return (1);
+}
+
+int		solve(int i, int *array, int n)
+{
+	int	sol_counter;
+	int	x;
+
+	if (i == n)
+	{
+		print_array(array, n);
+		return (1);
+	}
+	sol_counter = 0;
+	x = 0;
+	while (x < n)
+	{
+		array[i] = x;
+		if (check(array, i))
+			sol_counter += solve(i + 1, array, n); // backtracking algo 
+		x++;
+	}
+	return (sol_counter);
+}
+
+int		n_queens_puzzle(int n)
+{
+	int *array = malloc(sizeof(int) * n);
+	if (!array)
+		return (-1);
+	return (solve(0, array, n));
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("Usage: ./queenie N , where N is the chess board size (N x N)\n");
+		return (1);
+	}
+	int n = atoi(argv[1]);
+	printf("number of solutions: %d\n", n_queens_puzzle(n));
+}
